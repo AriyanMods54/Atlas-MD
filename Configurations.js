c920e1cf74cb25d38b4b6e1520d9d872526be0f8
AriyanModzz <133765043+AriyanMods54@users.noreply.github.com>
2023-07-20 17:27:47 +0500

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -14,7 +14,7 @@ global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuk
 global.packname = process.env.PACKNAME || `ARIYAN-BOT`;
 global.author = process.env.AUTHOR || "by: ARIYAN AHMED";
 global.port = process.env.PORT || "10000";
-global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
+global.openAiAPI = process.env.OPENAI_API || "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";
 global.owner = gg.split(",");
 
 module.exports = {
